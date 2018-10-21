a1196b2698818373721e18a37a86006af7918f07
Dmitry Sadovnikov <sadovnikov.js@gmail.com>
2018-10-21 22:53:02 +0300

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'users#index'
 
-  get '/auth/spotify/callback', to: 'users#show'
+  get '/auth/spotify/callback', to: 'users#spotify'
 
   resources :users, only: %i[index show] do
     get :logout
